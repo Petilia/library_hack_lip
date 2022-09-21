ce647ffdb2c634d3c52c365519ec7f65379b5299
petili <petryashin.i.e@mail.ru>
2022-09-21 16:20:54 +0300

    residial metaclip

---
+++ py_files/finetune_exp/metaclip_arch/metaclip_v4.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import numpy as np
+
+class ImageEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(768, 1380)
+        self.fc2 = nn.Linear(1380, 768)
+
+    def forward(self, input):
+        ratio = 0.2
+        x = F.relu(self.fc1(input))
+        x = self.fc2(x)
+        x = ratio * x + (1 - ratio) * input
+        return x
+
+class TextEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(768, 1380)
+        self.fc2 = nn.Linear(1380, 768)
+
+    def forward(self, input):
+        ratio = 0.2
+        x = F.relu(self.fc1(input))
+        x = self.fc2(x)
+        x = ratio * x + (1 - ratio) * input
+        return x
+
+class MetaCLIP(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.encode_image = ImageEncoder()
+        self.encode_text = TextEncoder()
+        self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
+
+    def forward(self, image, text):
+        #open_clip realization
+        image_features = self.encode_image(image)
+        image_features = F.normalize(image_features, dim=-1)
+
+        text_features = self.encode_text(text)
+        text_features = F.normalize(text_features, dim=-1)
+
+        return image_features, text_features, self.logit_scale.exp()
